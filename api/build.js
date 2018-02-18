61cf7fff3269ff75ccfdc34f9a36279210cfc0ad
mil <mil@spreadshirt.net>
2018-02-18 17:51:10 +0100

    fix flow in build script

--- api/build.js
+++ api/build.js
@@ -2,27 +2,33 @@
 import glob from 'glob';
 import fs from 'fs';
 
+// $FlowFixMe - extensions is defined
 require.extensions['.graphql'] = (module: Object, filename: string) => {
   // eslint-disable-next-line no-param-reassign
   module.exports = fs.readFileSync(filename, 'utf8');
 };
 
-const asyncGlob = async (
+const asyncGlob = (
   filePath: string,
   options: Object,
-): Promise<Array<string>> =>
+): Promise<Array<string> | void> =>
   new Promise((resolve: () => void, reject: (error: Error) => void) => {
     glob(filePath, options, (error: Error, files: Array<string>) => {
       if (error) {
         reject(error);
       }
 
+      // $FlowFixMe - resolve takes an argument
       resolve(files);
     });
   });
 
-const loadSchemas = async (): Array<string> => {
+const loadSchemas = async (): Promise<Array<string>> => {
   const files = await asyncGlob('src/**/*.graphql', {});
+
+  if (!files) {
+    throw new Error('no graphql files found');
+  }
 
   const filteredFiles = files.filter(
     (file: string): boolean => !file.includes('node_modules'),
@@ -35,13 +41,14 @@ const loadSchemas = async (): Array<string> => {
   });
 };
 
-const buildSchemaFile = async (): void => {
+const buildSchemaFile = async (): Promise<void> => {
   const schemas = await loadSchemas();
   const schemaString = schemas.join('');
 
   fs.writeFile(
     `${__dirname}/build/index.graphql`,
     schemaString,
+    // $FlowFixMe - function is fine
     (error: Error) => {
       if (error) {
         // eslint-disable-next-line no-console
