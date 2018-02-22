4d75b0517fa135b4076745e395845877ca077c55
mil <mil@spreadshirt.net>
2018-02-22 22:00:43 +0100

    add nested route

--- app/src/containers/About/index.js
+++ app/src/containers/About/index.js
@@ -1,6 +1,19 @@
 // @flow
-import React, { type Node } from 'react';
+import React, { type Node, Fragment } from 'react';
+import { Route, Link as RouterLink, type Props as RouterProps } from 'react-router-dom';
 
-const About = (): Node => <div>About</div>;
+import Home from '../Home';
+
+type Props = RouterProps;
+
+const About = ({ match }: Props): Node => (
+  <Fragment>
+    <div>About {match.url}</div>
+    <RouterLink to={`${match.url}/nested`}>link</RouterLink>
+
+    <Route exact path={`${match.url}`} render={() => <div>about...</div>} />
+    <Route path={`${match.url}/nested`} component={() => <h3>nested!</h3>} />
+  </Fragment>
+);
 
 export default About;
